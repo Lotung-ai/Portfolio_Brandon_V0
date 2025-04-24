2177e074e666573c7c3945ac5b5ed44a6a835f11
Lotung <Lotung@gmail.com>
2025-04-24 23:59:51 +0200

    Mise à jour des packages pour deployer le site

--- portfolio_brandon_v0/vite.config.js
+++ portfolio_brandon_v0/vite.config.js
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/Portfolio_Brandon_V0/',
     plugins: [plugin()],
     server: {
         port: 55131,
