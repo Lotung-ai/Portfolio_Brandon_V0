7d6f88228c520a404c2bff4554eef6c8c8603305
Lotung <Lotung@gmail.com>
2025-04-28 23:29:46 +0200

    Mise à jour

--- portfolio_brandon_v0/vite.config.js
+++ portfolio_brandon_v0/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/Portfolio_Brandon_V0/',
-    plugins: [plugin(), ghPages()],
+    plugins: [plugin()],
     server: {
         port: 55131,
     }
