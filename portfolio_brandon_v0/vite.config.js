85528c940d8bd2b31e34c14aa5aea61d32317bd6
Lotung <Lotung@gmail.com>
2025-04-28 23:05:18 +0200

    Ajout du plugin pour mettre à jour le gh-pages

--- portfolio_brandon_v0/vite.config.js
+++ portfolio_brandon_v0/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [plugin(), ghPages()],
     server: {
         port: 55131,
     }
